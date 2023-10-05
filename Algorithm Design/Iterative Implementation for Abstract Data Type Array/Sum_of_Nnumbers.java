d816c92c43a0a31e7c4ab5dad59c00e21fc931e1
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2023-10-06 02:12:42 +0530

    Update Sum_of_Nnumbers.java

--- Algorithm Design/Iterative Implementation for Abstract Data Type Array/Sum_of_Nnumbers.java
+++ Algorithm Design/Iterative Implementation for Abstract Data Type Array/Sum_of_Nnumbers.java
@@ -13,7 +13,7 @@ public class Sum_of_Nnumbers
 		int n=sc.nextInt();
 		int sum=0;
 		for(int i=1; i<=n; i++)
-			sum+=i;;
+			sum+=i;
 		System.out.println("Sum of n-numbers is: "+sum);
 		sc.close();
 
