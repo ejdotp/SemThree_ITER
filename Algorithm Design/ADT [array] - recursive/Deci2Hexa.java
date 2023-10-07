79cedd286d1e8d0cc4a9cf461686566c9f6a6c26
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2023-10-07 18:48:14 +0000

    deci2hexa

--- Algorithm Design/ADT [array] - recursive/Deci2Hexa.java
+++ Algorithm Design/ADT [array] - recursive/Deci2Hexa.java
@@ -1,16 +1,32 @@
-//covert decimal to hexadecimal using recursion:
+//covert decimal to hexadecimal using array and recursion in java?
 
-public class Deci2Hexa
+import java.util.Scanner;
+class Deci2Hexa
 {
+	char a[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+	 int rem;
+	String hexdec="";
+	 String hexadecimal(int q)
+	 {   
+   	 if(q != 0)
+    	    {
+		rem=q%16;
+		hexdec= a[rem] + hexdec;
+	               q= q/16;
+		 hexadecimal(q);  
+	   }
+        	return hexdec;
+	 }         
+    public static void main(String args[])
+    {
+       Deci2Hexa d=new Deci2Hexa();
+        int dec, rem, q;
+        Scanner sc = new Scanner(System.in);   
+        System.out.println("Enter a Decimal number: ");
+        dec=sc.nextInt();
+        System.out.println("Hexadecimal number is : ");
+        String h=d.hexadecimal(dec);    	       
+        System.out.print(h);
 
-	public static void convert(int n)
-	{
-		
-	}
-	public static void main(String[] args)
-	{
-		// TODO Auto-generated method stub
-
-	}
-
+     }    
 }
