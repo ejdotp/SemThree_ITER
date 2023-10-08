16da7128d41aa0f3a531d8912cdabc8eebe61f8e
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2023-10-08 14:47:37 +0500

    Update Deci2Hexa.java

--- Algorithm Design/ADT [array] - recursive/Deci2Hexa.java
+++ Algorithm Design/ADT [array] - recursive/Deci2Hexa.java
@@ -4,32 +4,39 @@ import java.util.Scanner;
 class Deci2Hexa
 {
 	char a[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-	 int rem;
+	int rem;
 	String hexdec="";
-	 String hexadecimal(int q)
-	 {   
-   	 if(q != 0)
-    	    {
-		rem=q%16;
-		hexdec= a[rem] + hexdec;
-	               q= q/16;
-		 hexadecimal(q);  
-	   }
-        	return hexdec;
-	 }         
+	String hexadecimal(int q)
+	{   
+		if(q != 0)
+		{
+			rem=q%16;
+			hexdec= a[rem] + hexdec;
+			q= q/16;
+			hexadecimal(q);  
+		}
+
+		return hexdec;
+	}         
     public static void main(String args[])
     {
-       Deci2Hexa d=new Deci2Hexa();
-        int dec, rem, q;
+        Deci2Hexa d=new Deci2Hexa();
+        int dec;
         Scanner sc = new Scanner(System.in);   
         System.out.print("Enter a Decimal number: ");
         dec=sc.nextInt();
         System.out.print("Hexadecimal number is: ");
         String h=d.hexadecimal(dec);    	       
         System.out.print(h);
+		sc.close();
 
      }    
 }
+
+/*Output:
+Enter a Decimal number: 60
+Hexadecimal number is: 3C
+*/
 
 /*Output:
 Enter a Decimal number: 60
