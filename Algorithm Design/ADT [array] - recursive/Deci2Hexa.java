2cf52b77c744bfe12e046e55b9e649ddd55565b4
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2023-10-12 23:55:08 +0500

    Update Deci2Hexa.java

--- Algorithm Design/ADT [array] - recursive/Deci2Hexa.java
+++ Algorithm Design/ADT [array] - recursive/Deci2Hexa.java
@@ -3,34 +3,33 @@
 import java.util.Scanner;
 class Deci2Hexa
 {
-	char a[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+	char[] a = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 	int rem;
-	String hexdec="";
+	String hexdec = "";
 	String hexadecimal(int q)
 	{   
 		if(q != 0)
 		{
-			rem=q%16;
-			hexdec= a[rem] + hexdec;
-			q= q/16;
+			rem = q%16;
+			hexdec = a[rem] + hexdec;
+			q = q/16;
 			hexadecimal(q);  
 		}
 
 		return hexdec;
 	}         
-    public static void main(String args[])
-    {
-        Deci2Hexa d=new Deci2Hexa();
-        int dec;
-        Scanner sc = new Scanner(System.in);   
-        System.out.print("Enter a Decimal number: ");
-        dec=sc.nextInt();
-        System.out.print("Hexadecimal number is: ");
-        String h=d.hexadecimal(dec);    	       
-        System.out.print(h);
+	public static void main(String args[])
+	{
+        	Deci2Hexa d = new Deci2Hexa();
+        	int dec;
+        	Scanner sc = new Scanner(System.in);   
+        	System.out.print("Enter a Decimal number: ");
+        	dec=sc.nextInt();
+        	System.out.print("Hexadecimal number is: ");
+        	String h=d.hexadecimal(dec);    	       
+        	System.out.print(h);
 		sc.close();
-
-     }    
+    }    
 }
 
 /*Output:
