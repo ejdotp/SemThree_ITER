322f31178af1a8c5595c8090e4d744e1499fb672
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2023-11-10 08:11:08 +0000

    add output

--- Algorithm Design/Atrangi Searches/RepeatFirst.java
+++ Algorithm Design/Atrangi Searches/RepeatFirst.java
@@ -1,6 +1,6 @@
 //Finding first repeated element in an array
-public class RepeatFirst {
-
+public class RepeatFirst
+{
 	public static int Repeat(int[] a)
 	{
 		for(int i=0; i<a.length; i++)
@@ -11,8 +11,11 @@ public class RepeatFirst {
 	}
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		int[] arr = {1, 2, 5, 3, 3, 5, 7, 9};
+		System.out.println("First repeated element is: "+Repeat(arr));
 	}
-
 }
+
+/*Output:
+ First repeated element is: 5
+ */
