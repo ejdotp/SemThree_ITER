37641ab87b6dd944946704f9aad75baaddbd48fc
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2023-11-10 09:14:06 +0000

    update q4

--- Algorithm Design/Atrangi Searches/FindMaxMinPair.java
+++ Algorithm Design/Atrangi Searches/FindMaxMinPair.java
@@ -5,6 +5,7 @@ public class FindMaxMinPair
     public static void main(String[] args)
     {
         int[] arr = {2, 4, 6, 3, 7, 11, 9};
+        Arrays.sort(arr);
         int[] minpair = new int[2];
         int[] maxpair = new int[2];
         int temp, diff=Integer.MAX_VALUE;
@@ -12,9 +13,7 @@ public class FindMaxMinPair
         {
             for(int j=i+1; j<arr.length; j++)
             {
-                temp = arr[i] - arr[j];
-                if(temp<0)
-                    temp=(-temp);
+                temp = arr[j] - arr[i];
                 if(diff>temp)
                 {
                     diff = temp;
@@ -24,7 +23,6 @@ public class FindMaxMinPair
             }
         }
         System.out.println("Min pair is"+Arrays.toString(minpair));
-        Arrays.sort(arr);
         maxpair[0] = arr[0];
         maxpair[1] = arr[arr.length-1];
         System.out.println("Max pair is"+Arrays.toString(maxpair));
--- Algorithm Design/Atrangi Searches/MaxCardinality.java
+++ Algorithm Design/Atrangi Searches/MaxCardinality.java
@@ -1,4 +1,5 @@
 //Given a list of n numbers, find element which appears the max no. of time
-public class MaxCardinality {
+public class MaxCardinality
+{
     
 }
