c768aa483b12568b38aa781ade260ede3d47c6d3
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2023-11-17 08:29:15 +0000

    linked list start

---
+++ Algorithm Design/Linked List/SingleLinkedList.java
@@ -0,0 +1,13 @@
+public class LinkedList{
+	static class Node{
+		int data;
+		Node next;
+		Node(int data)
+		{
+			this.data=data;
+			this.next=null;
+		}
+	}
+	public static Node head;
+	public statis Node tail;
+}
