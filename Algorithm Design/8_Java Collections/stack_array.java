4e3a4363a836119138e8550a67ae125e104935a1
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2023-11-24 08:09:13 +0000

    stack java

---
+++ Algorithm Design/8_Java Collections/stack_array.java
@@ -0,0 +1,7 @@
+//stack implementation using array(practice question, not part of this lab)
+import java.util.ArrayList;
+class StackArray
+{
+    int top;
+    int size;
+    ArrayList<Integer> stack;
