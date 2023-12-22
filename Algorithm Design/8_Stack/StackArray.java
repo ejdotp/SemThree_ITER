4373583e6530e7d11fa675e58685cb652bb3a5c9
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2023-12-22 08:21:06 +0000

    stack

--- Algorithm Design/8_Stack/StackArray.java
+++ Algorithm Design/8_Stack/StackArray.java
@@ -1,7 +1,48 @@
-//stack implementation using array(practice question, not part of this lab)
+//stack implementation using array
 import java.util.ArrayList;
 class StackArray
 {
-    int top;
-    int size;
-    ArrayList<Integer> stack;
+    static class Stack
+    {
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        public static boolean isEmpty()
+        {
+            return list.size() == 0;
+        }
+        
+        public static void push(int data)
+        {
+            list.add(data);
+        }
+
+        public static int pop()
+        {
+            if (isEmpty())
+                return -1;
+            int top = list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+
+        public static int peek()
+        {
+            if (isEmpty())
+                return -1;
+            return list.get(list.size()-1);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Stack.push(1);
+        Stack.push(2);
+        Stack.push(3);
+        while (!Stack.isEmpty())
+        {
+            System.out.println(Stack.peek());
+            Stack.pop();
+        }
+        System.out.println(Stack.peek());
+    }
+}
