2c9b3df9d6829641cd9f476b324816acee97954b
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2024-01-03 08:35:42 +0000

    x

--- Computer Science Workshop/Class Lectures/5_Java/Test.java
+++ Computer Science Workshop/Class Lectures/5_Java/Test.java
@@ -4,5 +4,8 @@ public class Test
     {
         System.out.println("Hello World!"); //moves the cursor to the next line then print
         System.out.print("Hello Another World!"); //prints wherever the cursor is
+        System.out.println(3+7); //prints 10
+        System.out.println(3*7); //prints 21
+        System.out.println("3+7"); //prints 3+7
     }
 }
