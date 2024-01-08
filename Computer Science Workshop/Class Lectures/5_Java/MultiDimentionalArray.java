f3ca4f717bedda8a353c556f157da520c7da8a1e
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2024-01-08 09:05:07 +0000

    x

--- Computer Science Workshop/Class Lectures/5_Java/MultiDimentionalArray.java
+++ Computer Science Workshop/Class Lectures/5_Java/MultiDimentionalArray.java
@@ -4,9 +4,9 @@ public class MultiDimentionalArray
     {
         int[][] numbers = {{1, 2, 3}, {4, 5, 6}};
         System.out.println(numbers[1][2]);
-        int k = numbers[2][2];
+        int k = numbers[1][2];
         System.out.println(k);
-        numbers[2][1] = 10;
-        System.out.println(numbers[2][1]);
+        numbers[1][1] = 10;
+        System.out.println(numbers[1][1]);
     }
 }
