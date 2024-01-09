65722d7f1a6bf1e804fafa519eff87cefa4f76ae
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2024-01-09 12:00:43 +0000

    X

--- Computer Science Workshop/Class Lectures/5_Java/Enums/EnumLoop.java
+++ Computer Science Workshop/Class Lectures/5_Java/Enums/EnumLoop.java
@@ -1,5 +1,13 @@
-package Enums;
-
-public class EnumLoop {
-    
+enum level
+{
+    LOW, MEDIUM, HIGH, E_HIGH
+}
+public class EnumLoop
+{
+    public static void main(String[] args) {
+        for(level myvar:level.values())
+        {
+            System.out.println(myvar);
+        }
+    }
 }
