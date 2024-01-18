61b5e79c6df83c784bbaed3b8d62834e4d4f7179
Jagadeeswar Patro <119095231+ejdotp@users.noreply.github.com>
2024-01-18 12:25:54 +0000

    X

--- Computer Science Workshop/Assignments/Assignment 5/Q10.java
+++ Computer Science Workshop/Assignments/Assignment 5/Q10.java
@@ -1,45 +1,42 @@
 import java.util.Scanner;
-
-public class Q10 {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+public class Q10
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter an expression (e.g., 123+345 or 5*10): ");
-        String input = scanner.nextLine();
-
-        input = input.replaceAll("\\s", "");
-
-        if (input.matches(".*[+\\-*/].*")) {
-            String[] parts = input.split("[+\\-*/]");
-
-            double operand1 = Double.parseDouble(parts[0]);
-            double operand2 = Double.parseDouble(parts[1]);
-
-            char operator = input.charAt(parts[0].length());
-
-            switch (operator) {
-                case '+':
-                    System.out.println("Sum = " + (operand1 + operand2));
-                    break;
-                case '-':
-                    System.out.println("Difference = " + (operand1 - operand2));
-                    break;
-                case '*':
-                    System.out.println("Product = " + (operand1 * operand2));
-                    break;
-                case '/':
-                    if (operand2 != 0) {
-                        System.out.println("Quotient = " + (operand1 / operand2));
-                    } else {
-                        System.out.println("Error: Division by zero is not allowed.");
-                    }
-                    break;
-                default:
-                    System.out.println("Invalid operator.");
+        String n = sc.nextLine();
+        char x='\0';
+        int i; double res=0;
+        for(i = 0; i<n.length(); i++)
+        {
+            if(n.charAt(i) == '+' || n.charAt(i) == '-' || n.charAt(i) == '*' || n.charAt(i) == '/')
+            {
+                x = n.charAt(i);
+                break;
             }
-        } else {
-            System.out.println("Invalid input. Please provide an expression with an operator (+, -, *, /).");
         }
+        String a = n.substring(0, i);
+        String b = n.substring(i+1, n.length());
+        Double a1 = Double.valueOf(a);
+        Double b1 = Double.valueOf(b);
+        switch(x)
+        {
+            case '+' :
+                res = a1+b1;
+                break;
+            case '-' :
+                res = a1-b1;
+                break;
+            case '*' :
+                res = a1*b1;
+                break;
+            case '/' :
+                res = a1/b1;
+                break;
+            default :
+                System.out.println("Bhai kar kya raha hai tu?");
+        }
+        System.out.println("Your Result: "+res);
     }
 }
